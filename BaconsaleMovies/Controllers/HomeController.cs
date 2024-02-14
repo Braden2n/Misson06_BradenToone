84f8028c5cb3081b828c8a943bbeaf7eb32c55c1
btoone <braden@toonetown.com>
2024-02-14 14:38:43 -0700

    Added handlers and context to make sqlite save info

--- BaconsaleMovies/Controllers/HomeController.cs
+++ BaconsaleMovies/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ namespace BaconsaleMovies.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext _movieContext;
+        public HomeController(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult AboutJoel()
         {
             return View();
@@ -21,11 +25,12 @@ namespace BaconsaleMovies.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult EnterMovie(Movie movie) 
         {
-            return View();
+            _movieContext.Add(movie);
+            _movieContext.SaveChanges();
+            return View(movie);
         }
     }
 }
