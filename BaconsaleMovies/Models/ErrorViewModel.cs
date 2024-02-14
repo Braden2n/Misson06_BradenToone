2ded4cce2dcb4ab9b0128626934ad6921c6df26b
btoone <braden@toonetown.com>
2024-02-14 13:07:29 -0700

    Removed Unnecessary Views

--- BaconsaleMovies/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BaconsaleMovies.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
