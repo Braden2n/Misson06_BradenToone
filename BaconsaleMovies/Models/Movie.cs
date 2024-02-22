dc0c00363b729147f2533e3fa573674ef523eee8
btoone <braden@toonetown.com>
2024-02-21 19:56:06 -0700

    Updated model to use validation instead of constructor requirements

--- BaconsaleMovies/Models/Movie.cs
+++ BaconsaleMovies/Models/Movie.cs
@@ -8,22 +8,22 @@ namespace BaconsaleMovies.Models
     {
         [Key]
         [Required(ErrorMessage = "MovieId is a required field")]
-        public required int MovieId { get; set; }
+        public int MovieId { get; set; }
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
         public CategoryModel? Category { get; set; }
         [Required(ErrorMessage = "Title is a required field")]
-        public required string Title { get; set; }
+        public string Title { get; set; }
         [Required(ErrorMessage = "Year is a required field")]
         [Range(1888, int.MaxValue, ErrorMessage = "The year must be 1888 or greater")]
-        public required int Year { get; set; }
+        public int Year { get; set; }
         public string? Director { get; set; }
         public string? Rating { get; set; }
         [Required(ErrorMessage = "Edited is a required field")]
-        public required bool Edited { get; set; } = false;
+        public bool Edited { get; set; } = false;
         public string? LentTo { get; set; }
         [Required(ErrorMessage = "CopiedToPlex is a required field")]
-        public required bool CopiedToPlex { get; set; } = false;
+        public bool CopiedToPlex { get; set; } = false;
         public string? Notes { get; set; }
     }
 }
